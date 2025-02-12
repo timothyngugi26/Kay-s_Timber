e04ec5022ec4553b975b3fb9c476a74f6188a194
timothyngugi26 <timothyngugi26gmail.com>
2025-02-12 08:33:21 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+import os
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
